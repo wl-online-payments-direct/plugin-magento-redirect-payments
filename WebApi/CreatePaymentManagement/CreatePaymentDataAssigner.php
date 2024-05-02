d4caf2642a02b42296ee25375280121bdd35f054
WorldlineMagentoTeam <plugin.worldline@amasty.com>
2024-05-02 14:28:19 +0300

    Release 1.15.0

--- WebApi/CreatePaymentManagement/CreatePaymentDataAssigner.php
+++ WebApi/CreatePaymentManagement/CreatePaymentDataAssigner.php
@@ -5,9 +5,11 @@ namespace Worldline\RedirectPayment\WebApi\CreatePaymentManagement;
 
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Quote\Api\Data\PaymentInterface;
+use Psr\Log\LoggerInterface;
 use Worldline\HostedCheckout\Api\TokenManagerInterface;
 use Worldline\HostedCheckout\Gateway\Request\PaymentDataBuilder;
 use Worldline\HostedCheckout\Service\HostedCheckout\CreateHostedCheckoutService;
+use Worldline\PaymentCore\Api\Data\QuotePaymentInterface;
 use Worldline\PaymentCore\Model\DataAssigner\DataAssignerInterface;
 use Worldline\RedirectPayment\Service\HostedCheckout\CreateHostedCheckoutRequestBuilder;
 
@@ -28,28 +30,39 @@ class CreatePaymentDataAssigner implements DataAssignerInterface
      */
     private $tokenManager;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     public function __construct(
         CreateHostedCheckoutService $createRequest,
         CreateHostedCheckoutRequestBuilder $createRequestBuilder,
-        TokenManagerInterface $tokenManager
+        TokenManagerInterface $tokenManager,
+        LoggerInterface $logger
     ) {
         $this->createRequest = $createRequest;
         $this->createRequestBuilder = $createRequestBuilder;
         $this->tokenManager = $tokenManager;
+        $this->logger = $logger;
     }
 
     /**
      * Assign return and payment id and identify redirect url
      *
      * @param PaymentInterface $payment
+     * @param QuotePaymentInterface $wlQuotePayment
      * @param array $additionalInformation
      * @return void
      * @throws LocalizedException
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function assign(PaymentInterface $payment, array $additionalInformation): void
-    {
+    public function assign(
+        PaymentInterface $payment,
+        QuotePaymentInterface $wlQuotePayment,
+        array $additionalInformation
+    ): void {
         $quote = $payment->getQuote();
 
         $token = $this->tokenManager->getToken($quote);
@@ -57,10 +70,23 @@ class CreatePaymentDataAssigner implements DataAssignerInterface
             return;
         }
 
+        $storedPayIds = $payment->getAdditionalInformation('payment_ids') ?? [];
+
         $request = $this->createRequestBuilder->build($quote);
         $response = $this->createRequest->execute($request, (int)$quote->getStoreId());
+        $paymentId = $response->getHostedCheckoutId();
         $payment->setAdditionalInformation('return_id', $response->getRETURNMAC());
-        $payment->setAdditionalInformation(PaymentDataBuilder::HOSTED_CHECKOUT_ID, $response->getHostedCheckoutId());
+        $payment->setAdditionalInformation('payment_ids', array_merge($storedPayIds, [$paymentId]));
+        $payment->setAdditionalInformation(PaymentDataBuilder::HOSTED_CHECKOUT_ID, $paymentId);
         $payment->setWlRedirectUrl($response->getRedirectUrl());
+        $wlQuotePayment->setPaymentIdentifier($paymentId);
+        $wlQuotePayment->setMethod($payment->getMethod());
+
+        if ($storedPayIds) {
+            $this->logger->warning(__(
+                'Another payment ID for quote was noticed: payment_id - '
+                . $paymentId . ', quote_id - ' . $quote->getId() . '.'
+            ));
+        }
     }
 }
